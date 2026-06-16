0c11a71eb3825fa811d063a406b7cd483514732a
90Theo <s225093@student.dtu.dk>
2026-06-16 15:39:49 +0200

    updated pdf's

---
+++ exercises/day6/theo/exercise8.py
@@ -0,0 +1,5 @@
+# Author: Theodor la Cour, s225093
+# Generative AI tools from Anthropic were used in the creation of this file.
+# They have been used for synthesizing, code structering, coding, and verification.
+# The author takes full responsibility for all content and decisions in this file.
+
